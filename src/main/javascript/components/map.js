5662ecf862d9ccb46ff169624a2dcbb9116b5bfd
JefBinomed <jean.francois.garreau@gmail.com>
2013-12-11 17:59:13 +0100

    modif

--- src/main/javascript/components/map.js
+++ src/main/javascript/components/map.js
@@ -10,7 +10,7 @@ components.directive('map', ['ModelFactory', 'GeoServicesFactory', '$rootScope',
     },    
     link: function postLink($scope, iElement, iAttrs) { 
 
-      var useGoogleMaps = google != null;
+      var useGoogleMaps = google != undefined;
       var mapDivElt = iElement.find('div')[0];
       var markers = [];
       var windows = [];
